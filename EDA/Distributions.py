958448e243f8a46e60245a5189485bbaab4c3620
Githika <gannapur@ucsc.edu>
2025-05-02 15:00:42 -0700

    code to create pie charts works!

--- EDA/Distributions.py
+++ EDA/Distributions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import seaborn as sns
 
 def encoding(codebook):
@@ -26,32 +27,36 @@ def pie_chart(df, codebook):
     feature_code_mappings = l[0]
     feature_domains = l[1]
     print(feature_code_mappings, "/n/n")
-    #features_to_be_plotted = ['MOMALIV2', 'MOMCOD2', 'STOPMEN2', 'SYMPTOM2', 'HOTFLA2', 'HEVYPER2', 'SLEEPRO2', 'PSYPROB2', 'OTHRPRO2', 'OTHRPRS2', 'HERMENO2', 'GENMENO2', 'ATTIMEN2',
-    #                          'ARTHRMO2', 'HIBPMOM2', 'DIABMOM2', 'STRKMOM2', 'HEARTMO2', 'OHDMOM2', 'HIPMOM2', 'OSTEOMO2', 'SPINEMO2', 'HUMPMOM2', 'COLONMO2', 'DEPRMOM2', 'DADALIV2' , 
-    #                          'ARTHRDA2', 'HIBPDAD2', 'DIABDAD2', 'STRKDAD2', 'HEARTDA2', 'OHDDAD2', 'HIPDAD2', 'OSTEODA2', 'SPINEDA2', 'HUMPDAD2', 'COLONDA2', 'DEPRDAD2']
-
-    features_to_be_plotted = ['MOMALIV2', 'STOPMEN2', 'SYMPTOM2', 'HOTFLA2', 'HEVYPER2', 'SLEEPRO2', 'PSYPROB2', 'OTHRPRO2', 'OTHRPRS2', 'HERMENO2', 'GENMENO2', 'ATTIMEN2',
+    features_to_be_plotted = ['MOMALIV2', 'MOMCOD2', 'STOPMEN2', 'SYMPTOM2', 'HOTFLA2', 'HEVYPER2', 'SLEEPRO2', 'PSYPROB2', 'OTHRPRO2', 'OTHRPRS2', 'HERMENO2', 'GENMENO2', 'ATTIMEN2',
                               'ARTHRMO2', 'HIBPMOM2', 'DIABMOM2', 'STRKMOM2', 'HEARTMO2', 'OHDMOM2', 'HIPMOM2', 'OSTEOMO2', 'SPINEMO2', 'HUMPMOM2', 'COLONMO2', 'DEPRMOM2', 'DADALIV2' , 
                               'ARTHRDA2', 'HIBPDAD2', 'DIABDAD2', 'STRKDAD2', 'HEARTDA2', 'OHDDAD2', 'HIPDAD2', 'OSTEODA2', 'SPINEDA2', 'HUMPDAD2', 'COLONDA2', 'DEPRDAD2']
 
+
+    colors = ['#ec4899', '#fbcfe8', '#d1d5db', '#6b7280', '#000000']
     for i in features_to_be_plotted:    
         if i not in feature_code_mappings:
             continue
         label_map = feature_code_mappings[i]
 
         # Count the occurrences of each value (Only count positive values because negative values include Not applicable and I don't know)
-        counts = df[i][df[i] >= 0].value_counts().reindex(feature_domains[i], fill_value=0)
+        counts = pd.to_numeric(df[i], errors='coerce')[lambda x: x >= 0].value_counts().reindex(feature_domains[i], fill_value=0)
 
-        # Extract counts and matching labels
-        values = counts.values
-        labels = [label_map[val] for val in counts.index]
+        counts_for_pie = counts[counts.index > 0]
+        
+        values_for_pie = counts_for_pie.values 
+
+        labels = [f"{label_map[val]} ({counts[val]})" for val in counts_for_pie.index]
+
 
         # Plot
-        plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=90)
+        plt.pie(values_for_pie, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
         plt.title(f'{i} Chart')
         plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        plt.legend(loc="lower left")
-        plt.savefig(f'./{i}_distribution.png')
+        legend_labels = [f"{label_map[val]}: {counts[val]}" for val in counts.index]
+        patches = [mpatches.Patch(color=colors[j], label=legend_labels[j]) for j in range(len(legend_labels))]
+        plt.legend(handles=patches, loc='lower left')
+        plt.savefig(f'./pie_charts/{i}_distribution.png')
+        plt.clf()  # Clear figure for next plot
 
 # for features we want to find a distribution of 
 # ex. contains 'years' as a measurement
